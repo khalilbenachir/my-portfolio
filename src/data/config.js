4a98647d98664cade1cb420fd19ac4c973f77832
khalil benachir <kbenachir@sqli.com>
2020-03-02 16:13:01 +0100

    get data from github repository using graphql

---
+++ src/data/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  defaultTitle: 'Benachir',
+  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  author: 'Khalil benahcir',
+  url: 'https://portfolio.smakosh.com',
+  legalName: 'John Doe',
+  defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/khalilbenachir',
+    github: 'https://github.com/khalilbenachir',
+    linkedin: 'https://ma.linkedin.com/in/khalil-benachir-797228155/',
+    instagram: 'https://instagram.com/khalilbenachir',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'khalil benachir',
+    twitter: 'Khalil benachir',
+  },
+  address: {
+    city: 'OUJDA',
+    region: 'OUJDA',
+    country: 'MOROCCO',
+    zipCode: '60000',
+  },
+  contact: {
+    email: 'kbenachir@sqli.com',
+    phone: '+212600491602',
+  },
+  foundingDate: '2019',
+  SITE_RECAPTCHA_KEY: '6Lf-990UAAAAAMUg65o_bWVC4iA4PHSBQIpUsdmT',
+  SITE_RECAPTCHA_SECRET: '6Lf-990UAAAAAFd-w_hSxEJ7CMqe98BPtX097rXi',
+};
