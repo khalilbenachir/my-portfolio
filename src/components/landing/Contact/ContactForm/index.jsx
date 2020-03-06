2cd3897c87f6efc61ded9f9e2d999840a73d7d53
khalil benachir <kbenachir@sqli.com>
2020-03-06 16:23:05 +0100

    fix google recaptcha issues by using react-captcha

--- src/components/landing/Contact/ContactForm/index.jsx
+++ src/components/landing/Contact/ContactForm/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Form, withFormik, FastField, ErrorMessage } from 'formik';
-import Recaptcha from 'react-google-recaptcha';
-import * as Yup from 'yup';
 import { Button, Input } from 'components/common';
 import { SITE_RECAPTCHA_KEY } from 'data/config';
+import Recaptcha from 'react-recaptcha';
 import { Error, Center, InputField } from './styles';
 
 const ContactForm = ({ setFieldValue, isSubmitting, values, errors, touched }) => (
@@ -53,6 +52,17 @@ const ContactForm = ({ setFieldValue, isSubmitting, values, errors, touched }) =
       />
       <ErrorMessage component={Error} name="message" />
     </InputField>
+    {values.name && values.email && values.message && (
+      <InputField>
+        <FastField
+          component={Recaptcha}
+          sitekey={SITE_RECAPTCHA_KEY}
+          name="recaptcha"
+          onChange={value => setFieldValue('recaptcha', value)}
+        />
+        <ErrorMessage component={Error} name="recaptcha" />
+      </InputField>
+    )}
     {values.success && (
       <InputField>
         <Center>
@@ -73,6 +83,7 @@ export default withFormik({
     name: '',
     email: '',
     message: '',
+    recaptcha: '',
     success: false,
   }),
   validationSchema: () =>
@@ -82,8 +93,9 @@ export default withFormik({
         .email('Invalid email')
         .required('Email field is required'),
       message: Yup.string().required('Message field is required'),
+      recaptcha: Yup.string().required('Robots are not welcome yet!'),
     }),
-  handleSubmit: async ({ name, email, message }, { setSubmitting, resetForm, setFieldValue }) => {
+  handleSubmit: async ({ name, email, message, recaptcha }, { setSubmitting, resetForm, setFieldValue }) => {
     try {
       const encode = data =>
         Object.keys(data)
@@ -97,6 +109,7 @@ export default withFormik({
           name,
           email,
           message,
+          'g-recaptcha-response': recaptcha,
         }),
       });
       await setSubmitting(false);
